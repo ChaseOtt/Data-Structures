c3453d3f4d8ead9619a5f59a864338182f5452fe
Jackson Golding <89219498+jacksongolding@users.noreply.github.com>
2022-03-29 14:14:00 -0700

    Update lcm.py

--- Crossover/lcm.py
+++ Crossover/lcm.py
@@ -1,1 +1,21 @@
+def lcm(x,y):
+  if (x>y):
+    n = x
+  else:
+    n = y
+  while (True):
+    if (n % x == 0 and n % y == 0):
+      break
+    n = n + 1
+  return n
 
+def lcmTest():
+  try:
+    x = int(input("Enter the first number: "))
+    y = int(input("Enter the second number: "))
+    if x < 0 or y < 0:
+      raise ValueError
+    else:
+      print(lcm(x,y))
+  except ValueError:
+    print("Please only input values greater than 0.")
