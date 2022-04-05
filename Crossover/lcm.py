1d792e41b3189f86f7b5ef25c52c7646975cc920
Jackson Golding <89219498+jacksongolding@users.noreply.github.com>
2022-04-05 05:58:35 +0000

    Added comments to lcm

--- Crossover/lcm.py
+++ Crossover/lcm.py
@@ -1,8 +1,10 @@
 def lcm(x,y):
+  # Determines which number to add to in the function
   if (x>y):
     n = x
   else:
     n = y
+  # Determines if their is a remainder when dividing n by x and y
   while (True):
     if (n % x == 0 and n % y == 0):
       break
@@ -10,6 +12,7 @@ def lcm(x,y):
   return n
 
 def lcmTest():
+  # Takes an input from the user of the numbers being put in the LCM function
   try:
     x = int(input("Enter the first number: "))
     y = int(input("Enter the second number: "))
@@ -17,5 +20,6 @@ def lcmTest():
       raise ValueError
     else:
       print("The Lowest Common Multiple of", x, "and", y, "is", lcm(x,y))
+  # Gives and error message if the user puts in invalid numbers for x and/or y
   except ValueError:
     print("Please only input values greater than 0.")
